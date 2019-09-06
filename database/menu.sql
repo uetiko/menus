2055e340c56f4a42ec3c58367a31207804b66e74
Angel Barrientos <angel@uetiko.com>
2019-09-06 13:51:30 -0500

    Added varchar to id tables for use uuid

--- database/menu.sql
+++ database/menu.sql
@@ -19,3 +19,12 @@ CREATE TABLE examen.manu_relationship (
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8
 COLLATE=utf8_general_ci;
+
+ALTER TABLE examen.manu_relationship MODIFY COLUMN id VARCHAR(40) NOT NULL;
+ALTER TABLE examen.manu_relationship DROP FOREIGN KEY manu_relationship_FK_1;
+ALTER TABLE examen.manu_relationship DROP FOREIGN KEY manu_relationship_FK;
+ALTER TABLE examen.menu MODIFY COLUMN id VARCHAR(40) NOT NULL;
+ALTER TABLE examen.manu_relationship MODIFY COLUMN id_parent VARCHAR(40) NOT NULL;
+ALTER TABLE examen.manu_relationship MODIFY COLUMN id_child VARCHAR(40) NOT NULL;
+ALTER TABLE examen.manu_relationship ADD CONSTRAINT manu_relationship_FK FOREIGN KEY (id_parent) REFERENCES examen.menu(id);
+ALTER TABLE examen.manu_relationship ADD CONSTRAINT manu_relationship_FK_1 FOREIGN KEY (id_child) REFERENCES examen.menu(id);
